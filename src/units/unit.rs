6b752e98016a140186db2f516a1ec64e7c24cfe5
Gary Holland <garyhollandxyz@gmail.com>
2022-05-21 16:43:21 +0100

    wip: basic knight sprite

--- src/units/unit.rs
+++ src/units/unit.rs
@@ -4,10 +4,10 @@ use crate::tile_map::{Map, Pos};
 
 #[derive(Default)]
 pub(crate) struct UnitSprites {
-    pub(crate) man_north_east: Handle<Image>,
-    pub(crate) man_north_west: Handle<Image>,
-    pub(crate) man_south_east: Handle<Image>,
-    pub(crate) man_south_west: Handle<Image>,
+    pub(crate) knight_north_east: Handle<Image>,
+    pub(crate) knight_north_west: Handle<Image>,
+    pub(crate) knight_south_east: Handle<Image>,
+    pub(crate) knight_south_west: Handle<Image>,
 }
 
 #[derive(Component)]
@@ -28,33 +28,32 @@ impl Plugin for UnitPlugin {
 
 impl UnitPlugin {
     fn load_unit_graphics(asset_server: Res<AssetServer>, mut graphics: ResMut<UnitSprites>) {
-        let man_north_east_handle =
-            asset_server.load::<Image, &str>("units/man/man_north_east.png");
-        let man_north_west_handle =
-            asset_server.load::<Image, &str>("units/man/man_north_west.png");
-        let man_south_east_handle =
-            asset_server.load::<Image, &str>("units/man/man_south_east.png");
-        let man_south_west_handle =
-            asset_server.load::<Image, &str>("units/man/man_south_west.png");
+        let knight_north_east_handle =
+            asset_server.load::<Image, &str>("units/knight/knight_north_east.png");
+        let knight_north_west_handle =
+            asset_server.load::<Image, &str>("units/knight/knight_north_west.png");
+        let knight_south_east_handle =
+            asset_server.load::<Image, &str>("units/knight/knight_south_east.png");
+        let knight_south_west_handle =
+            asset_server.load::<Image, &str>("units/knight/knight_south_west.png");
 
-        graphics.man_north_east = man_north_east_handle;
-        graphics.man_north_west = man_north_west_handle;
-        graphics.man_south_east = man_south_east_handle;
-        graphics.man_south_west = man_south_west_handle;
+        graphics.knight_north_east = knight_north_east_handle;
+        graphics.knight_north_west = knight_north_west_handle;
+        graphics.knight_south_east = knight_south_east_handle;
+        graphics.knight_south_west = knight_south_west_handle;
     }
 
     fn spawn_unit(mut commands: Commands, graphics: ResMut<UnitSprites>, map_query: Query<&Map>) {
         let map = map_query.get_single().expect("Not exactly 1 map");
-        let player_starting_pos = Pos::new(6.0, 0.0, 5.0);
-        // let player_starting_pos = Pos::new(1.0, 0.5, 0.0);
+        let starting_pos = Pos::new(6.0, 0.0, 0.0);
 
-        let tile_entity = map.tiles.get(&player_starting_pos).expect("No such tile");
-        let screen_coords = map.world_pos_to_unit_screen_pos_absolute(player_starting_pos);
+        let tile_entity = map.tiles.get(&starting_pos).expect("No such tile");
+        let screen_coords = map.world_pos_to_unit_screen_pos_absolute(starting_pos);
 
         commands
             .spawn_bundle(SpriteBundle {
                 transform: Transform::from_translation(screen_coords),
-                texture: graphics.man_south_east.clone(),
+                texture: graphics.knight_north_west.clone(),
                 sprite: Sprite {
                     // roughly the sprite's feet
                     anchor: Anchor::Custom(Vec2::new(0.0, -0.4)),
@@ -64,7 +63,7 @@ impl UnitPlugin {
             })
             .insert(Unit {
                 tile: *tile_entity,
-                pos: player_starting_pos,
+                pos: starting_pos,
             });
     }
 }
